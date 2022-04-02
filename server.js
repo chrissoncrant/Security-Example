c5c53c6b45fea259ea4d9e727a3b7e00500be525
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-02 11:29:01 -0700

    set up skeleton endpoints for Google OAuth; simple UI setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+const helmet = require('helmet');
 
 const express = require('express');
 
@@ -8,9 +9,36 @@ const PORT = 3000;
 
 const app = express();
 
-app.get('/secret', (req, res) => {
-    res.send('Your personal secret value is: 42')
+app.use(helmet());
+
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true //TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: "You must log in!"
+        })
+    };
+    next();
+}
+
+//This is where a user is taken when they click login:
+app.get('/auth/google', (req, res) => {
+
 });
+
+//This is where the OAuth google callback is returned to, server side, to receive the Authorization Code from Google. The response is the Authorization Code plus the client secret which was established when the server was registered with the Google Authorization Server in Video 217:
+app.get('/auth/google/callback', (req, res) => {
+
+});
+
+//How a user logs out
+app.get('/auth/logout', (req, res) => {
+
+});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+    return res.send('Your personal secret is 42!');
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
